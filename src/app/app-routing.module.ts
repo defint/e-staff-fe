8c3e35a08671bb0a18dd4dbd0e1fea19073f9180
Anton <anton.matrenin@introduct.tech>
2020-03-02 06:57:29 +0200

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { NotFoundComponent } from "./not-found/not-found.component";
 import { EmployeeCreateComponent } from "./employee/employee-create/employee-create.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+  { path: "", redirectTo: "/employee", pathMatch: "full" },
   { path: "dashboard", component: DashboardPageComponent },
   {
     path: "employee",
